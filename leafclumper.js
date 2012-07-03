0f32da1ad9c3efc0c9693d545f9d57f183573258
cavis <ryancavis@gmail.com>
2012-07-02 21:36:26 -0500

    convert tabs to spaces (whoops!)

--- leafclumper.js
+++ leafclumper.js
@@ -25,101 +25,106 @@
  * groups based on screen spacing, updating with every zoom level.
  * ========================================================== */
 L.MarkerClumper = L.Class.extend({
-	options: {
-		radius: 60
-	},
+    options: {
+        radius: 60
+    },
 
-	initialize: function(marks) {
-		this._markers = {};
-		this._clumps  = {};
-		if (marks) {
-			for (var i=0; i<marks.length; i++) {
-				this.addMarker(marks[i]);
-			}
-		}
-	},
+    initialize: function(marks) {
+        this._markers = {};
+        this._clumps  = {};
+        if (marks) {
+            for (var i=0; i<marks.length; i++) {
+                this.addMarker(marks[i]);
+            }
+        }
+    },
 
-	addMarker: function(mark) {
-		var id = L.Util.stamp(mark);
-		this._markers[id] = mark;
-		if (this._map) {
-			this._clumpMarker(mark);
-		}
-		return this;
-	},
+    addMarker: function(mark) {
+        var id = L.Util.stamp(mark);
+        this._markers[id] = mark;
+        if (this._map) {
+            this._clumpMarker(mark);
+        }
+        return this;
+    },
 
-	removeMarker: function(mark) {
-		var id = L.Util.stamp(mark);
-		delete this._markers[id];
+    removeMarker: function(mark) {
+        var id = L.Util.stamp(mark);
+        delete this._markers[id];
 
-		if (this._map) {
-			this._map.removeLayer(mark);
-		}
+        if (this._map) {
+            this._map.removeLayer(mark);
+        }
 
-		return this;
-	},
+        return this;
+    },
 
-	clear: function() {
-		this._iterateClumps(this._map.removeLayer, this._map);
-		this._clumps = {};
-		return this;
-	},
+    clear: function() {
+        this._iterateClumps(this._map.removeLayer, this._map);
+        this._clumps = {};
+        return this;
+    },
 
-	onAdd: function(map) {
-		this._map = map;
-		this._map.on('zoomend', this.onZoomEnd, this);
-		this._iterateMarkers(this._clumpMarker, this);
-	},
+    onAdd: function(map) {
+        this._map = map;
+        this._map.on('zoomend', this.onZoomEnd, this);
+        this._iterateMarkers(this._clumpMarker, this);
+    },
 
-	onRemove: function(map) {
-		this._iterateClumps(map.removeLayer, map);
-		this._map = null;
-	},
+    onRemove: function(map) {
+        this._iterateClumps(map.removeLayer, map);
+        this._map = null;
+    },
 
-	onZoomEnd: function(e) {
-		this.clear();
-		this._iterateMarkers(this._clumpMarker, this);
-	},
+    onZoomEnd: function(e) {
+        this.clear();
+        this._iterateMarkers(this._clumpMarker, this);
+    },
 
-	_clumpMarker: function(mark) {
-		var point = this._map.latLngToLayerPoint(mark.getLatLng()),
-		    clump = null, leastDistance = null;
-		for (var i in this._clumps) {
-			var dist = this._clumps[i].inBounds(point);
-			if (dist !== false && (!clump || dist < leastDistance)) {
-				clump = this._clumps[i];
-				leastDistance = dist;
-			}
-		}
+    onClumpClick: function(e, thing) {
+        console.log("ON CLUMP CLICK", e, thing);
+    },
 
-		// add or create
-		if (clump) {
-			clump.addMarker(mark);
-		}
-		else {
-			var clump = new L.Clump(mark, {radius: this.options.radius});
-			var id = L.Util.stamp(clump);
-			this._clumps[id] = clump;
-			this._map.addLayer(clump);
-		}
-		return this;
-	},
+    _clumpMarker: function(mark) {
+        var point = this._map.latLngToLayerPoint(mark.getLatLng()),
+            clump = null, leastDistance = null;
+        for (var i in this._clumps) {
+            var dist = this._clumps[i].inBounds(point);
+            if (dist !== false && (!clump || dist < leastDistance)) {
+                clump = this._clumps[i];
+                leastDistance = dist;
+            }
+        }
 
-	_iterateMarkers: function(method, context) {
-		for (var i in this._markers) {
-			if (this._markers.hasOwnProperty(i)) {
-				method.call(context, this._markers[i]);
-			}
-		}
-	},
+        // add or create
+        if (clump) {
+            clump.addMarker(mark);
+        }
+        else {
+            var clump = new L.Clump(mark, {radius: this.options.radius});
+            var id = L.Util.stamp(clump);
+            this._clumps[id] = clump;
+            this._clumps[id].on('click', this.onClumpClick, this);
+            this._map.addLayer(clump);
+        }
+        return this;
+    },
 
-	_iterateClumps: function(method, context) {
-		for (var i in this._clumps) {
-			if (this._clumps.hasOwnProperty(i)) {
-				method.call(context, this._clumps[i]);
-			}
-		}
-	}
+    _iterateMarkers: function(method, context) {
+        for (var i in this._markers) {
+            if (this._markers.hasOwnProperty(i)) {
+                method.call(context, this._markers[i]);
+            }
+        }
+    },
+
+    _iterateClumps: function(method, context) {
+        for (var i in this._clumps) {
+            if (this._clumps.hasOwnProperty(i)) {
+                method.call(context, this._clumps[i]);
+            }
+        }
+    }
 });
 
 
@@ -129,80 +134,80 @@ L.MarkerClumper = L.Class.extend({
  * An icon used for the clumping marker
  * ========================================================== */
 L.Icon.Clump = L.Icon.extend({
-	options: {
-		size: null
-	},
+    options: {
+        size: null
+    },
 
-	_sizeOptions: {
-		1: {
-			image: L.Icon.Default.prototype._getIconUrl('icon'),
-			iconSize: L.Icon.Default.prototype.options.iconSize,
-			iconAnchor: L.Icon.Default.prototype.options.iconAnchor,
-			popupAnchor: L.Icon.Default.prototype.options.popupAnchor,
-			shadowSize: null
-		},
-		2: {
-			image: '../lc1.png',
-			iconSize: new L.Point(53, 52),
-			iconAnchor: new L.Point(27, 26),
-			popupAnchor: new L.Point(0, 0),
-			shadowSize: null
-		},
-		10: {
-			image: '../lc1.png',
-			iconSize: new L.Point(53, 52),
-			iconAnchor: new L.Point(27, 26),
-			popupAnchor: new L.Point(0, 0),
-			shadowSize: null
-		},
-		20: {
-			image: '../lc1.png',
-			iconSize: new L.Point(53, 52),
-			iconAnchor: new L.Point(27, 26),
-			popupAnchor: new L.Point(0, 0),
-			shadowSize: null
-		}
-	},
+    _sizeOptions: {
+        1: {
+            image: L.Icon.Default.prototype._getIconUrl('icon'),
+            iconSize: L.Icon.Default.prototype.options.iconSize,
+            iconAnchor: L.Icon.Default.prototype.options.iconAnchor,
+            popupAnchor: L.Icon.Default.prototype.options.popupAnchor,
+            shadowSize: null
+        },
+        2: {
+            image: '../lc1.png',
+            iconSize: new L.Point(53, 52),
+            iconAnchor: new L.Point(27, 26),
+            popupAnchor: new L.Point(0, 0),
+            shadowSize: null
+        },
+        10: {
+            image: '../lc1.png',
+            iconSize: new L.Point(53, 52),
+            iconAnchor: new L.Point(27, 26),
+            popupAnchor: new L.Point(0, 0),
+            shadowSize: null
+        },
+        20: {
+            image: '../lc1.png',
+            iconSize: new L.Point(53, 52),
+            iconAnchor: new L.Point(27, 26),
+            popupAnchor: new L.Point(0, 0),
+            shadowSize: null
+        }
+    },
 
-	initialize: function(size) {
-		this._setClumpOptions(size || 1);
-	},
+    initialize: function(size) {
+        this._setClumpOptions(size || 1);
+    },
 
-	update: function(size, div) {
-		this._setClumpOptions(size);
-		this._setIconStyles(div, size)
-	},
+    update: function(size, div) {
+        this._setClumpOptions(size);
+        this._setIconStyles(div, size)
+    },
 
-	createIcon: function() {
-		var div = document.createElement('div');
-		this._setIconStyles(div);
-		return div;
-	},
+    createIcon: function() {
+        var div = document.createElement('div');
+        this._setIconStyles(div);
+        return div;
+    },
 
-	createShadow: function() {
-		return null; // TODO: something else?
-	},
+    createShadow: function() {
+        return null; // TODO: something else?
+    },
 
-	_setClumpOptions: function(size) {
-		for (var i in this._sizeOptions) {
-			if (size <= i) {
-				this.options = this._sizeOptions[i];
-				break;
-			}
-		}
-	},
+    _setClumpOptions: function(size) {
+        for (var i in this._sizeOptions) {
+            if (size <= i) {
+                this.options = this._sizeOptions[i];
+                break;
+            }
+        }
+    },
 
-	_setIconStyles: function(div, size) {
-		L.Icon.prototype._setIconStyles.call(this, div, 'icon');
-		div.style['cursor'] = 'pointer';
-		div.style['background'] = 'url('+this.options.image+') no-repeat 0 0';
-		div.style['text-align'] = 'center';
-		div.style['font-size'] = '12px';
-		div.style['font-weight'] = 'bold';
-		div.style['color'] = 'white';
-		div.style['line-height'] = this.options.iconSize.y+'px';
-		div.innerHTML = (size > 1 ) ? size : '';
-	}
+    _setIconStyles: function(div, size) {
+        L.Icon.prototype._setIconStyles.call(this, div, 'icon');
+        div.style['cursor'] = 'pointer';
+        div.style['background'] = 'url('+this.options.image+') no-repeat 0 0';
+        div.style['text-align'] = 'center';
+        div.style['font-size'] = '12px';
+        div.style['font-weight'] = 'bold';
+        div.style['color'] = 'white';
+        div.style['line-height'] = this.options.iconSize.y+'px';
+        div.innerHTML = (size > 1 ) ? size : '';
+    }
 });
 
 
@@ -212,70 +217,72 @@ L.Icon.Clump = L.Icon.extend({
  * A marker representing a clump of other markers
  * ========================================================== */
 L.Clump = L.Marker.extend({
-	options: {
-		radius: 60
-	},
+    options: {
+        radius: 60
+    },
 
-	initialize: function(mark, options) {
-		this._markers = [mark];
-		this._latlng = mark.getLatLng();
-		L.Util.setOptions(this, options);
-		this.options.icon = new L.Icon.Clump(1);
-	},
+    initialize: function(mark, options) {
+        this._markers = [mark];
+        this._latlng = mark.getLatLng();
+        L.Util.setOptions(this, options);
+        this.options.icon = new L.Icon.Clump(1);
+    },
 
-	onAdd: function(map) {
-		L.Marker.prototype.onAdd.call(this, map);
-		this._center = this._map.latLngToLayerPoint(this._latlng);
-	},
+    onAdd: function(map) {
+        L.Marker.prototype.onAdd.call(this, map);
+        this._center = this._map.latLngToLayerPoint(this._latlng);
+    },
 
-	inBounds: function(point) {
-		var dist = this._center.distanceTo(point);
-		return (dist < this.options.radius ? dist : false);
-	},
+    inBounds: function(point) {
+        var dist = this._center.distanceTo(point);
+        return (dist < this.options.radius ? dist : false);
+    },
 
-	addMarker: function(mark) {
-		this._markers.push(mark);
-		this.options.icon.update(this._markers.length, this._icon);
+    addMarker: function(mark) {
+        this._markers.push(mark);
+        this.options.icon.update(this._markers.length, this._icon);
 
-		// weighted average the point location
-		var weight = this._markers.length;
-		var point = this._map.latLngToLayerPoint(mark.getLatLng());
-		this._center = new L.Point(
-			(this._center.x * (weight-1) + point.x) / weight,
-			(this._center.y * (weight-1) + point.y) / weight
-		);
-		this.setLatLng(this._map.layerPointToLatLng(this._center));
-	},
+        // weighted average the point location
+        var weight = this._markers.length;
+        var point = this._map.latLngToLayerPoint(mark.getLatLng());
+        this._center = new L.Point(
+            (this._center.x * (weight-1) + point.x) / weight,
+            (this._center.y * (weight-1) + point.y) / weight
+        );
+        this.setLatLng(this._map.layerPointToLatLng(this._center));
+    },
 
-	_setMapPosition: function() {
-		var avg = [0, 0];
-		for (var i=0; i<this._markers.length; i++) {
-			var point = this._map.latLngToLayerPoint(mark.getLatLng());
-			avg[0] += point.x;
-			avg[1] += point.y;
-		}
-		this._center = new L.Point(
-			avg[0] / this._markers.length,
-			avg[1] / this._markers.length
-		);
-		this.setLatLng(this._map.layerPointToLatLng(this._center));
-	},
+    _setMapPosition: function() {
+        var avg = [0, 0];
+        for (var i=0; i<this._markers.length; i++) {
+            var point = this._map.latLngToLayerPoint(mark.getLatLng());
+            avg[0] += point.x;
+            avg[1] += point.y;
+        }
+        this._center = new L.Point(
+            avg[0] / this._markers.length,
+            avg[1] / this._markers.length
+        );
+        this.setLatLng(this._map.layerPointToLatLng(this._center));
+    },
 
-	_onMouseClick: function (e) {
-		L.DomEvent.stopPropagation(e);
-		if (this._markers.length > 1) {
-			var all = [];
-			for (var i=0; i<this._markers.length; i++) {
-				all.push(this._markers[i].getLatLng());
-			}
-			this._map.fitBounds(new L.LatLngBounds(all));
-		}
-		else {
-			this.fire(e.type, {
-				originalEvent: e
-			});
-		}
-	}
+    _onMouseClick: function (e) {
+        L.DomEvent.stopPropagation(e);
+
+        // zoom in when multiple are clicked
+        if (this._markers.length > 1) {
+            var all = [];
+            for (var i=0; i<this._markers.length; i++) {
+                all.push(this._markers[i].getLatLng());
+            }
+            this._map.fitBounds(new L.LatLngBounds(all));
+        }
+        this.fire(e.type, {
+            originalEvent: e,
+            markers: this._markers,
+            zooming: (this._markers.length > 1)
+        });
+    }
 
 });
 
